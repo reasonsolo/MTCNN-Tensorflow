6f4c7858c1abf587910e7d2390772f64dfc2167f
reasonsolo <reasonsolo@gmail.com>
2018-06-12 19:10:56 +0800

    fix gen_hard_examples.py

--- prepare_data/gen_tfrecords.py
+++ prepare_data/gen_tfrecords.py
@@ -10,7 +10,7 @@ from train_models.MTCNN_config import config
 
 LANDMARK_LEN = config.LANDMARK_SIZE * 2
 DATA_TYPES = ['pos', 'neg', 'part', 'landmark']
-BASE_IMGLIST_NAME = 'train_%s_landmark.txt'
+BASE_IMGLIST_NAME = '%s_%s.txt'
 
 def _add_to_tfrecord(filename, image_record, tfrecord_writer):
     image_data, height, width = _process_image_withoutcoder(filename)
@@ -28,16 +28,29 @@ def line_to_example(i, line, dataset_path):
     bbox['xmax'] = 0
     bbox['ymax'] = 0
     data_example['landmark'] = [0] * LANDMARK_LEN
+    label = data_example['label'] 
     if len(segs) == 6:
         bbox['xmin'] = float(segs[2])
         bbox['ymin'] = float(segs[3])
         bbox['xmax'] = float(segs[4])
         bbox['ymax'] = float(segs[5])
-    elif len(segs) == LANDMARK_LEN + 2:
+    if label == -2:
         data_example['landmark'] = [float(lm[0:5]) for lm in segs[2:]]
+	if len(data_example['landmark']) != LANDMARK_LEN:
+            print('data line %d landmark length error %d' % (i, len(data_example['landmark'])))
+	    return None
+        if all(v == 0.0 for v in data_example['landmark']):
+            print('all zero line %d' % i)
+	    return None
         for lm in data_example['landmark']:
             if lm < 0 or lm > 1:
                 print("invalid landmark %f" % lm)
+		return None
+    elif len(segs) == 6:
+        bbox['xmin'] = float(segs[2])
+        bbox['ymin'] = float(segs[3])
+        bbox['xmax'] = float(segs[4])
+        bbox['ymax'] = float(segs[5])
     elif len(segs) == 2:
         # negative record
         pass
@@ -69,19 +82,24 @@ def gen_tfrecord_file(dataset_path, tfrecord_path, shuffling=True):
     with tf.python_io.TFRecordWriter(tfrecord_path) as tfrecord_writer:
         for i, data_example in enumerate(dataset):
             filename = data_example['filename']
-            print('>> Converting image %d/%d %s' % (i+1, total_size, filename))
+            # print('>> Converting image %d/%d %s' % (i+1, total_size, filename))
             _add_to_tfrecord(filename, data_example, tfrecord_writer)
 
 def gen_pnet_tfrecord(dataset_dir, output_dir, net='PNet'):
-    file_name = BASE_IMGLIST_NAME % net
-    dataset_path = os.path.join(dataset_dir, file_name)
-    tfrecord_path = '%s/train_%s_landmark.tfrecord' % (output_dir, net)
-    gen_tfrecord_file(dataset_path, tfrecord_path)
+    for data_type in DATA_TYPES:
+        file_name = BASE_IMGLIST_NAME % (data_type, net)
+        dataset_path = os.path.join(dataset_dir, file_name)
+        tfrecord_path = '%s/%s_landmark.tfrecord' % (output_dir, data_type)
+        gen_tfrecord_file(dataset_path, tfrecord_path)
+    #file_name = BASE_IMGLIST_NAME % net
+    #dataset_path = os.path.join(dataset_dir, file_name)
+    #tfrecord_path = '%s/train_%s_landmark.tfrecord' % (output_dir, net)
+    #gen_tfrecord_file(dataset_path, tfrecord_path)
 
 
 def gen_rnet_tfrecord(dataset_dir, output_dir, net='RNet'):
     for data_type in DATA_TYPES:
-        file_name = BASE_IMGLIST_NAME % net
+        file_name = BASE_IMGLIST_NAME % (data_type, net)
         dataset_path = os.path.join(dataset_dir, file_name)
         tfrecord_path = '%s/%s_landmark.tfrecord' % (output_dir, data_type)
         print('dataset %s tfrecord %s' % (dataset_path, tfrecord_path))
@@ -89,7 +107,7 @@ def gen_rnet_tfrecord(dataset_dir, output_dir, net='RNet'):
 
 def gen_onet_tfrecord(dataset_dir, output_dir, net='ONet'):
     for data_type in DATA_TYPES:
-        file_name = BASE_IMGLIST_NAME % net
+        file_name = BASE_IMGLIST_NAME % (data_type, net)
         dataset_path = os.path.join(dataset_dir, file_name)
         tfrecord_path = '%s/%s_landmark.tfrecord' % (output_dir, data_type)
         gen_tfrecord_file(dataset_path, tfrecord_path)
@@ -99,11 +117,11 @@ if __name__ == '__main__':
     net = sys.argv[1]
     imglist_dir = 'imglists/%s' % net
     if net == 'PNet':
-        gen_pnet_tfrecord(imglist_dir, imglist_dir)
+        gen_pnet_tfrecord(net, imglist_dir)
     elif net == 'RNet':
-        gen_rnet_tfrecord(imglist_dir, imglist_dir)
+        gen_rnet_tfrecord(net, imglist_dir)
     elif net == 'ONet':
-        gen_onet_tfrecord(imglist_dir, imglist_dir)
+        gen_onet_tfrecord(net, imglist_dir)
     else:
         print('invalid net %s' % net)
 
